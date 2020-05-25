53e1106ec325d9a8f2493cea88a49f01912e4199
Dmytro Arkhypov <dimonarh@gmail.com>
2020-05-25 14:07:49 +0700

    todo items made as a components, add properties important, id and label

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import AppHeader from './components/app-header';
 import SearchPanel from './components/search-panel';
 import TodoList from './components/todo-list';
 
+const todoData = [
+  { label: 'Drink Cofee', important: false, id:1 },
+  { label: 'Crate Project', important: true, id:2 },
+  { label: 'Lunch', important: false, id:3 }
+   
+];
+
 const App = () => {
   return (
     <div>
       <AppHeader />
       <SearchPanel />
-      <TodoList />
+      <TodoList todos={todoData}/>
     </div>
   );
 };
